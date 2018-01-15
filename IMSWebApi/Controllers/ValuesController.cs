9e0a9403263031010640f02156965d5604556821
amolpatil855 <amol.patil@eternussolutions.com>
2018-01-15 16:18:59 +0530

    Cahngs done for solution name change

---
+++ IMSWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IMSWebApi.CustomAttributes;
+
+namespace IMSWebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [Authorize]
+        [ApiAuthorize(AccessLevel ="All")]
+        public IEnumerable<string> Get()
+        {
+            int b = 0;
+            int a = 5 / b;
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        // POST api/values
+        public int Add(int id,string name)
+        {
+            return id;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ IMSWebApi/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using IMSWebApi.Models;
+
+namespace IMSWebApi.Providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (WebAPIdbEntities _repo = new WebAPIdbEntities())
+            {
+                User user = _repo.Users.FirstOrDefault(p => p.username.Equals(context.UserName) && p.password.Equals(context.Password));
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            if (context.UserName.Equals("amol"))
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+            else
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+            context.Validated(identity);
+
+        }
+    }
+}
