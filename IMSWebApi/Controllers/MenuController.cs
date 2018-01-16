2af671371a9d85b372c80c10ef53844fc88a2685
Raj Mungra <rajr.mungra@gmail.com>
2018-01-16 19:38:17 +0530

    Updated MenuController conflicts

--- IMSWebApi/Controllers/MenuController.cs
+++ IMSWebApi/Controllers/MenuController.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using IMSWebApi.Services;
 using System;
 using System.Collections.Generic;
@@ -24,30 +24,3 @@ namespace IMSWebApi.Controllers
         }
     }
 }
-=======
-﻿using IMSWebApi.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace IMSWebApi.Controllers
-{
-    public class MenuController : ApiController
-    {
-        private MenuService _menuService = null;
-        public MenuController()
-        {
-            _menuService = new MenuService();
-        }
-        // GET: api/Menu
-        public IHttpActionResult Get()
-        {
-            var result = _menuService.getMenu();
-            return Ok(result);
-        }
-    }
-}
->>>>>>> 89cbb17f6e907a295629af3e9f901a750073264a
