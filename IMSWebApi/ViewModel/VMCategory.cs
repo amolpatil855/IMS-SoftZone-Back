1ddf2e144fbb478f7b5545543773158db9bb900e
Raj Mungra <rajr.mungra@gmail.com>
2018-01-19 19:01:27 +0530

    ViewModel of category Added

---
+++ IMSWebApi/ViewModel/VMCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMSWebApi.ViewModel
+{
+    public class VMCategory
+    {
+        public long id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public long createdBy { get; set; }
+        public Nullable<long> updatedBy { get; set; }
+    }
+}
