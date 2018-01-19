05017e7165f59c1812c43d2b44cf4ffd5a97e21c
Raj Mungra <rajr.mungra@gmail.com>
2018-01-19 20:40:52 +0530

    Created structure for Design API

---
+++ IMSWebApi/Controllers/DesignController.cs
@@ -0,0 +1,21 @@
+﻿using IMSWebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IMSWebApi.Controllers
+{
+    public class DesignController : ApiController
+    {
+        private DesignService _designService = null;
+
+        public DesignController()
+        {
+            _designService = new DesignService();
+        }
+
+    }
+}
---
+++ IMSWebApi/ViewModel/VMDesign.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMSWebApi.ViewModel
+{
+    public class VMDesign
+    {
+        public long id { get; set; }
+        public long categoryId { get; set; }
+        public long collectionId { get; set; }
+        public long qualityId { get; set; }
+        public string designCode { get; set; }
+        public string designName { get; set; }
+        public string description { get; set; }
+        public long createdBy { get; set; }
+        public Nullable<long> updatedBy { get; set; }
+
+        public virtual VMCategory MstCategory { get; set; }
+        public virtual VMCollection MstCollection { get; set; }
+        
+    }
+}
