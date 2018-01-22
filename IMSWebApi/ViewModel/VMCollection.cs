533e1ed235b59dae80e11572956a04a68e9e94b8
Raj Mungra <rajr.mungra@gmail.com>
2018-01-22 18:51:02 +0530

    Annotations added for Validation of ViewModel

--- IMSWebApi/ViewModel/VMCollection.cs
+++ IMSWebApi/ViewModel/VMCollection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,14 +9,21 @@ namespace IMSWebApi.ViewModel
     public class VMCollection
     {
         public long id { get; set; }
+
+        [Required]
         public long categoryId { get; set; }
+        [Required]
         public long supplierId { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string collectionCode { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string collectionName { get; set; }
+        [MaxLength(500)]
         public string description { get; set; }
+        [MaxLength(100)]
         public string manufacturerName { get; set; }
-        public long createdBy { get; set; }
-        public Nullable<long> updatedBy { get; set; }
         public string categoryCode { get; set; }
 
         public virtual VMSupplier MstSupplier { get; set; }
