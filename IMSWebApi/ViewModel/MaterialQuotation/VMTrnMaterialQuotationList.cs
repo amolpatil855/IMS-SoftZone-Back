4632b5e2cc581918695c94ee54be855d41f85fd5
Raj Mungra <rajr.mungra@gmail.com>
2018-03-22 17:47:13 +0530

    Updated MQ Lookup for AdvancePayment

--- IMSWebApi/ViewModel/MaterialQuotation/VMTrnMaterialQuotationList.cs
+++ IMSWebApi/ViewModel/MaterialQuotation/VMTrnMaterialQuotationList.cs
@@ -7,9 +7,10 @@ namespace IMSWebApi.ViewModel
 {
     public class VMTrnMaterialQuotationList
     {
-        public long id { get; set; }
+        public long quotationId { get; set; }
         public string materialQuotationNumber { get; set; }
         public System.DateTime materialQuotationDate { get; set; }
+        public long customerId { get; set; }
         public string customerName { get; set; }
         public string status { get; set; }
         public int totalAmount { get; set; }
