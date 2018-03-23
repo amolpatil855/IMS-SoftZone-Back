58ca80e00908503d3f4b86f103112dd36be5130e
Raj Mungra <rajr.mungra@gmail.com>
2018-03-23 17:16:07 +0530

    Mail notification to Admin for cancelled PO and issue in PO post

--- IMSWebApi/Common/SendEmail.cs
+++ IMSWebApi/Common/SendEmail.cs
@@ -15,7 +15,8 @@ namespace IMSWebApi.Common
         public static string _smtpAddress =ConfigurationManager.AppSettings["SMTPHost"];
         public static string _emailFrom = ConfigurationManager.AppSettings["SMTPUserName"];
         public static string _password = ConfigurationManager.AppSettings["SMTPPassword"];
-
+        
+        //User registration and forgot password notification for user
         public void email(MstUser result,string originalPassword,string fileName,bool isResetPassword)
         {
             StringBuilder sbEmailDetails = new StringBuilder();
@@ -51,6 +52,7 @@ namespace IMSWebApi.Common
             ReusableEmailComponent.DAOFactoryProvider.GetEmailDao().sendMail(objEmail); 
         }
 
+        //When PO is created, Nofity Admin for PO created
         public void notificationForPO(VMTrnPurchaseOrder purchaseOrder, string fileName,MstUser loggedInUser,string adminEmail)
         {
             StringBuilder sbEmailDetails = new StringBuilder();
@@ -95,14 +97,122 @@ namespace IMSWebApi.Common
             ReusableEmailComponent.DAOFactoryProvider.GetEmailDao().sendMail(objEmail);
         }
 
+        //When PO approved, Notifiy Supplier for PO approved
+        public void notifySupplierForPO(VMTrnPurchaseOrder purchaseOrder, string fileName, string supplierEmail)
+        {
+            string shippingAddress = purchaseOrder.shippingAddress != null ? purchaseOrder.shippingAddress : purchaseOrder.MstCompanyLocation.addressLine1 + " " + purchaseOrder.MstCompanyLocation.addressLine2 +
+                                                   "," + purchaseOrder.MstCompanyLocation.city + ", " + purchaseOrder.MstCompanyLocation.state + " PINCODE - "
+                                                   + purchaseOrder.MstCompanyLocation.pin;
+
+            StringBuilder sbEmailDetails = new StringBuilder();
+            sbEmailDetails.AppendLine(System.IO.File.ReadAllText(HttpContext.Current.Server.MapPath(@"~\EmailTemplate\" + fileName + ".html")));
+
+            sbEmailDetails = sbEmailDetails.Replace("@courierMode", purchaseOrder.courierMode);
+            sbEmailDetails = sbEmailDetails.Replace("@supplierName", purchaseOrder.supplierName != null ? purchaseOrder.supplierName : purchaseOrder.MstSupplier.name);
+            sbEmailDetails = sbEmailDetails.Replace("@shippingAddress", shippingAddress);
+            sbEmailDetails = sbEmailDetails.Replace("@courierName", purchaseOrder.courierName != null ? purchaseOrder.courierName : purchaseOrder.MstCourier.name);
+
+            string rows = "";
+
+            foreach (var poItem in purchaseOrder.TrnPurchaseOrderItems)
+            {
+                string serialOrSize = poItem.serialno != null && poItem.serialno != "" ? poItem.serialno :
+                    poItem.size != null && poItem.size != "" ? poItem.size :
+                    poItem.shadeId != null ? poItem.MstFWRShade.serialNumber + "(" + poItem.MstFWRShade.shadeCode + ")" :
+                    poItem.matSizeId != null ? poItem.MstMatSize.sizeCode + " (" + poItem.MstMatSize.MstMatThickNess.thicknessCode + "-" + poItem.MstMatSize.MstQuality.qualityCode + ")" :
+                    poItem.fomSizeId != null ? poItem.MstFomSize.itemCode : poItem.matSizeCode;
+                string accessoryCode = poItem.accessoryName != null && poItem.accessoryName != "" ? poItem.accessoryName :
+                    poItem.accessoryId != null ? poItem.MstAccessory.itemCode : "";
+                string collectionName = poItem.collectionName != null && poItem.collectionName != "" ? poItem.collectionName :
+                    poItem.collectionId != null ? poItem.MstCollection.collectionName + " (" + poItem.MstCollection.MstSupplier.code + ")" : "";
+                string categoryName = poItem.categoryName != null && poItem.categoryName != "" ? poItem.categoryName : poItem.MstCategory.name;
+                rows += "<tr><td>" + categoryName
+                    + "</td><td> " + collectionName
+                    + "</td><td> " + serialOrSize
+                    + "</td><td> " + accessoryCode
+                    + "</td><td> " + poItem.orderQuantity
+                    + "</td><td> " + poItem.orderType
+                    + "</td><td> " + poItem.rate
+                    + "</td><td> " + poItem.amountWithGST
+                    + "</td></tr>";
+            }
+            sbEmailDetails = sbEmailDetails.Replace("@rows", rows);
+
+            EmailProperties objEmail = new EmailProperties();
+            objEmail.SmtpAddress = _smtpAddress;
+            objEmail.EmailFrom = _emailFrom;
+            objEmail.Password = _password;
+            objEmail.EmailTo.Add(supplierEmail);
+            objEmail.Subject = "Purchase Order Generated";
+            objEmail.EnableSSL = true;
+            objEmail.Body = sbEmailDetails.ToString();
+            objEmail.isBodyHtml = true;
+            objEmail.EnableSSL = true;
+            ReusableEmailComponent.DAOFactoryProvider.GetEmailDao().sendMail(objEmail);
+        }
+
+        //When PO is cancelled, Notify Admin For PO cancelled
+        public void notifyAdminForCancelledPO(TrnPurchaseOrder purchaseOrder, string fileName, string adminEmail)
+        {
+            StringBuilder sbEmailDetails = new StringBuilder();
+            sbEmailDetails.AppendLine(System.IO.File.ReadAllText(HttpContext.Current.Server.MapPath(@"~\EmailTemplate\" + fileName + ".html")));
+
+            string shippingAddress = purchaseOrder.MstCompanyLocation != null ? (purchaseOrder.MstCompanyLocation.addressLine1 + "," + (purchaseOrder.MstCompanyLocation.addressLine2 != null ?  purchaseOrder.MstCompanyLocation.addressLine2 + "," : "") +
+                purchaseOrder.MstCompanyLocation.city + "," + purchaseOrder.MstCompanyLocation.state + " PINCODE - " + purchaseOrder.MstCompanyLocation.pin) : null;
+
+            sbEmailDetails = sbEmailDetails.Replace("@poNumber", purchaseOrder.orderNumber);
+            sbEmailDetails = sbEmailDetails.Replace("@courierMode", purchaseOrder.courierMode);
+            sbEmailDetails = sbEmailDetails.Replace("@supplierName", purchaseOrder.MstSupplier != null ? purchaseOrder.MstSupplier.name : null);
+            sbEmailDetails = sbEmailDetails.Replace("@shippingAddress",  shippingAddress);
+            sbEmailDetails = sbEmailDetails.Replace("@courierName", purchaseOrder.MstCourier != null ? purchaseOrder.MstCourier.name : null);
+
+            string rows = "";
+
+            foreach (var poItem in purchaseOrder.TrnPurchaseOrderItems)
+            {
+                //string serialOrSize = poItem.shadeId != null ? poItem.serialno : poItem.fomSizeId != null ? poItem.size : poItem.matSizeId != null ? poItem.size : poItem.accessoryId;
+                string serialOrSize = poItem.shadeId != null ? poItem.MstFWRShade.serialNumber + "(" + poItem.MstFWRShade.shadeCode + ")" :
+                    poItem.matSizeId != null ? poItem.MstMatSize.sizeCode + " (" + poItem.MstMatSize.MstMatThickness.thicknessCode + "-" + poItem.MstMatSize.MstQuality.qualityCode + ")" :
+                    poItem.fomSizeId != null ? poItem.MstFomSize.itemCode : poItem.matSizeCode;
+                string accessoryCode = poItem.accessoryId != null ? poItem.MstAccessory.itemCode : "";
+                string collectionName = poItem.collectionId != null ? poItem.MstCollection.collectionName + " (" + poItem.MstCollection.MstSupplier.code + ")" : "";
+                string categoryName = poItem.MstCategory.name;
+                rows += "<tr><td>" + categoryName
+                    + "</td><td> " + collectionName
+                    + "</td><td> " + serialOrSize
+                    + "</td><td> " + accessoryCode
+                    + "</td><td> " + poItem.orderQuantity
+                    + "</td><td> " + poItem.orderType
+                    + "</td><td> " + poItem.rate
+                    + "</td><td> " + poItem.amountWithGST
+                    + "</td></tr>";
+            }
+            sbEmailDetails = sbEmailDetails.Replace("@rows", rows);
+
+            EmailProperties objEmail = new EmailProperties();
+            objEmail.SmtpAddress = _smtpAddress;
+            objEmail.EmailFrom = _emailFrom;
+            objEmail.Password = _password;
+            objEmail.EmailTo.Add(adminEmail);
+            objEmail.Subject = "Purchase Order Cancelled";
+            objEmail.EnableSSL = true;
+            objEmail.Body = sbEmailDetails.ToString();
+            objEmail.isBodyHtml = true;
+            objEmail.EnableSSL = true;
+            ReusableEmailComponent.DAOFactoryProvider.GetEmailDao().sendMail(objEmail);
+        }
+
+        //Notify Admin when SO created by other users
         public void notificationForSO(VMTrnSaleOrder saleOrder, string fileName, MstUser loggedInUser, string adminEmail)
         {
             StringBuilder sbEmailDetails = new StringBuilder();
             sbEmailDetails.AppendLine(System.IO.File.ReadAllText(HttpContext.Current.Server.MapPath(@"~\EmailTemplate\" + fileName + ".html")));
 
-            string shippingAddress = saleOrder.shippingAddress.addressLine1 + ", " + (saleOrder.shippingAddress.addressLine2 != null ? saleOrder.shippingAddress.addressLine2 + ", " : "" )+
-                    saleOrder.shippingAddress.city + ", " + saleOrder.shippingAddress.state + "Pincode : " + saleOrder.shippingAddress.pin;
-
+            string shippingAddress = saleOrder.shippingAddress != null ? saleOrder.shippingAddress.addressLine1 + ", " + (saleOrder.shippingAddress.addressLine2 != null ? saleOrder.shippingAddress.addressLine2 + ", " : "" )+
+                saleOrder.shippingAddress.city + ", " + saleOrder.shippingAddress.state + "Pincode : " + saleOrder.shippingAddress.pin :
+            saleOrder.MstCustomerAddress != null ? (saleOrder.MstCustomerAddress.addressLine1 + ", " + (saleOrder.MstCustomerAddress.addressLine2 != null ? saleOrder.MstCustomerAddress.addressLine2 + ", " : "") +
+            saleOrder.MstCustomerAddress.city + ", " + saleOrder.MstCustomerAddress.state + "Pincode : " + saleOrder.MstCustomerAddress.pin) : null;
+                
             sbEmailDetails = sbEmailDetails.Replace("@user", loggedInUser.userName);
             sbEmailDetails = sbEmailDetails.Replace("@courierMode", saleOrder.courierMode);
             sbEmailDetails = sbEmailDetails.Replace("@customerName", saleOrder.customerName);
@@ -140,59 +250,7 @@ namespace IMSWebApi.Common
             ReusableEmailComponent.DAOFactoryProvider.GetEmailDao().sendMail(objEmail);
         }
 
-        public void notifySupplierForPO(VMTrnPurchaseOrder purchaseOrder, string fileName,string supplierEmail)
-        {
-            string shippingAddress = purchaseOrder.shippingAddress!=null ? purchaseOrder.shippingAddress : purchaseOrder.MstCompanyLocation.addressLine1 + " " + purchaseOrder.MstCompanyLocation.addressLine2 +
-                                                   "," + purchaseOrder.MstCompanyLocation.city + ", " + purchaseOrder.MstCompanyLocation.state + " PINCODE - "
-                                                   + purchaseOrder.MstCompanyLocation.pin;
-
-            StringBuilder sbEmailDetails = new StringBuilder();
-            sbEmailDetails.AppendLine(System.IO.File.ReadAllText(HttpContext.Current.Server.MapPath(@"~\EmailTemplate\" + fileName + ".html")));
-
-            sbEmailDetails = sbEmailDetails.Replace("@courierMode", purchaseOrder.courierMode);
-            sbEmailDetails = sbEmailDetails.Replace("@supplierName", purchaseOrder.supplierName!=null ? purchaseOrder.supplierName : purchaseOrder.MstSupplier.name);
-            sbEmailDetails = sbEmailDetails.Replace("@shippingAddress", shippingAddress);
-            sbEmailDetails = sbEmailDetails.Replace("@courierName", purchaseOrder.courierName!=null ? purchaseOrder.courierName : purchaseOrder.MstCourier.name);
-
-            string rows = "";
-
-            foreach (var poItem in purchaseOrder.TrnPurchaseOrderItems)
-            {
-                string serialOrSize = poItem.serialno != null && poItem.serialno != ""? poItem.serialno :
-                    poItem.size != null && poItem.size != "" ? poItem.size :
-                    poItem.shadeId != null ? poItem.MstFWRShade.serialNumber + "(" + poItem.MstFWRShade.shadeCode + ")" :
-                    poItem.matSizeId != null ? poItem.MstMatSize.sizeCode + " (" + poItem.MstMatSize.MstMatThickNess.thicknessCode + "-" + poItem.MstMatSize.MstQuality.qualityCode + ")" :
-                    poItem.fomSizeId != null ? poItem.MstFomSize.itemCode : poItem.matSizeCode;
-                string accessoryCode = poItem.accessoryName != null && poItem.accessoryName != ""? poItem.accessoryName :
-                    poItem.accessoryId != null ? poItem.MstAccessory.itemCode : "";
-                string collectionName = poItem.collectionName != null && poItem.collectionName != ""? poItem.collectionName :
-                    poItem.collectionId != null ? poItem.MstCollection.collectionName + " (" + poItem.MstCollection.MstSupplier.code + ")": "";
-                string categoryName = poItem.categoryName != null && poItem.categoryName != ""? poItem.categoryName : poItem.MstCategory.name;
-                rows += "<tr><td>" + categoryName
-                    + "</td><td> " + collectionName
-                    + "</td><td> " + serialOrSize
-                    + "</td><td> " + accessoryCode
-                    + "</td><td> " + poItem.orderQuantity
-                    + "</td><td> " + poItem.orderType
-                    + "</td><td> " + poItem.rate
-                    + "</td><td> " + poItem.amountWithGST
-                    + "</td></tr>";
-            }
-            sbEmailDetails = sbEmailDetails.Replace("@rows", rows);
-
-            EmailProperties objEmail = new EmailProperties();
-            objEmail.SmtpAddress = _smtpAddress;
-            objEmail.EmailFrom = _emailFrom;
-            objEmail.Password = _password;
-            objEmail.EmailTo.Add(supplierEmail);
-            objEmail.Subject = "Purchase Order Generated";
-            objEmail.EnableSSL = true;
-            objEmail.Body = sbEmailDetails.ToString();
-            objEmail.isBodyHtml = true;
-            objEmail.EnableSSL = true;
-            ReusableEmailComponent.DAOFactoryProvider.GetEmailDao().sendMail(objEmail);
-        }
-
+        //Whwn SO is approved, notifiy its customer about approved SO
         public void approvedSONotificationForCustomer(VMTrnSaleOrder saleOrder, string fileName,string customerEmail)
         {
             StringBuilder sbEmailDetails = new StringBuilder();
@@ -248,6 +306,7 @@ namespace IMSWebApi.Common
             ReusableEmailComponent.DAOFactoryProvider.GetEmailDao().sendMail(objEmail);
         }
 
+        //When SO is cancelled, notify customer about cancelled SO
         public void cancelledSONotificationForCustomer(VMTrnSaleOrder saleOrder, string fileName)
         {
             StringBuilder sbEmailDetails = new StringBuilder();
@@ -297,6 +356,7 @@ namespace IMSWebApi.Common
             ReusableEmailComponent.DAOFactoryProvider.GetEmailDao().sendMail(objEmail);
         }
 
+        //Notification to Admin for Pending GIN containing items received in GRN
         public void notificationForPendingGIN(string grnNumber, string fileName, List<string> ginNumbers, string adminEmail)
         {
             StringBuilder sbEmailDetails = new StringBuilder();
